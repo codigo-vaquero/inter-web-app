10cd9c26a13bd80c45e380c19e0ab1a6544ed001
codigo-vaquero <joseantob85@gmail.com>
2026-01-19 15:40:03 -0800

    Access routes configured.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { AnotherComponent } from './components/another-component/another-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'another', component: AnotherComponent}
+];
