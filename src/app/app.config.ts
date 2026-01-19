ba439b5c14a1652810cec61c4f9aecf52b922d2f
codigo-vaquero <joseantob85@gmail.com>
2026-01-19 15:37:39 -0800

    ngx-translate lib confiured.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,38 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAppInitializer } from '@angular/core';
+
+import {
+  provideTranslateService
+} from '@ngx-translate/core';
+
+import {
+  provideTranslateHttpLoader
+} from '@ngx-translate/http-loader';
+
+import { LanguageService } from './services/language-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    provideTranslateService({
+      defaultLanguage: 'en',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      })
+    }),
+
+    provideAppInitializer(() => {
+      const langService = inject(LanguageService);
+      langService.init();
+    })
   ]
 };
